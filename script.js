a826abd11ffddda217e8027a57d4e1c620490a96
ajaz-mohammad <ajazmohammad2003@gmail.com>
2025-11-27 20:48:58 +0530

    buttons redesign

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// Hamburger Menu Design Started
-let hamburger = document.querySelector(".hamburger");
-let ul = document.querySelector(".navbar a");
-
-hamburger.addEventListener("click", () => {
-    ul.classList.toggle("showData");
-
-    if(ul.className.contains("showData")){
-        document.getElementById("bar").className="fa-regular fa-thumbs-up";
-    }
-    else{
-        document.getElementById("bar").className="fa-solid fa-bars";
-    }
-})
-// Hamburger Menu Design Ended
 
 function showSidebar(){
     const sidebar = document.querySelector('.sidebar')
