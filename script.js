f3c9c237e1d3ec771d0932c3be71717a7546b983
ajaz-mohammad <ajazmohammad2003@gmail.com>
2025-12-02 20:15:44 +0530

    Hamburger designed

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 
 
 //Hamburger Menu Design Started
-const hamburger = document.querySelector('.header .nav-bar .hamburger');
-const nav = document.querySelector('.header .nav-bar nav');
-hamburger.addEventListener('click', () => {
-	nav.classList.toggle('nav-toggle');
-	hamburger.classList.toggle('close');
+const navLinks = document.getElementById("navLinks");
+const openMenu = document.getElementById("openMenu");
+const closeMenu = document.getElementById("closeMenu");
+
+// Open menu
+openMenu.addEventListener("click", () => {
+    navLinks.classList.add("active");
+    openMenu.style.display = "none";
+    closeMenu.style.display = "block";
 });
-console.log(hamburger);
+
+// Close menu
+closeMenu.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+    closeMenu.style.display = "none";
+    openMenu.style.display = "block";
+});
+
+
 //Hamburger Menu Design Ended
 
 
