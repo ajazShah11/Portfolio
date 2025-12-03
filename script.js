b3724ea1e98a6640c2aeeef245c97ff723de81e6
ajaz-mohammad <ajazmohammad2003@gmail.com>
2025-12-03 20:32:06 +0530

    home & about responsive done

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
-
-
 //Hamburger Menu Design Started
 const navLinks = document.getElementById("navLinks");
 const openMenu = document.getElementById("openMenu");
 const closeMenu = document.getElementById("closeMenu");
 
-// Open menu
 openMenu.addEventListener("click", () => {
     navLinks.classList.add("active");
     openMenu.style.display = "none";
     closeMenu.style.display = "block";
 });
 
-// Close menu
 closeMenu.addEventListener("click", () => {
     navLinks.classList.remove("active");
     closeMenu.style.display = "none";
     openMenu.style.display = "block";
 });
-
-
 //Hamburger Menu Design Ended
 
 
